2459fcfa9d7ee7a95124a126e32c6e09c0868f9b
toanphp <tadangtoan@hotmail.com>
2016-11-04 18:02:59 +0700

    4/112

--- wp-content/themes/leflair/framework/post.options.php
+++ wp-content/themes/leflair/framework/post.options.php
@@ -1,7 +1,7 @@
 <?php
 
 $prefix = 'product_';
-$fields_page = array(		
+$product_fields = array(		
 	array( 
 		'label'	=> 'Header Slider', 
 		'desc'	=> '', 
@@ -84,6 +84,31 @@ $fields_page = array(
 	
 );	
 
+
+
+$prefix_page = 'page_';
+$page_fields = array(		
+	array( 
+		'label'	=> 'Header Image', 
+		'desc'	=> '', 
+		'id'	=> $prefix_page.'header_image', 
+		'type'	=> 'image',
+	),		
+	array( 
+		'label'	=> 'Title', 
+		'desc'	=> '', 
+		'id'	=> $prefix_page.'title', 
+		'type'	=> 'text',
+	),		
+	array( 
+		'label'	=> 'Subtitle', 
+		'desc'	=> '', 
+		'id'	=> $prefix_page.'subtitle', 
+		'type'	=> 'text',
+	),
+	
+);	
+
 /**
  * Instantiate the class with all variables to create a meta box
  * var $id string meta box id
@@ -95,6 +120,7 @@ $fields_page = array(
 require_once (THEME_PATH . "/framework/metaboxes/meta_box.php");
 //$restaurent_meta_box = new custom_add_meta_box( 'restaurant_meta_box', 'Restaurant info', $fields, 'restaurant', true );
 //$product_meta_box = new custom_add_meta_box( 'product_meta_box', 'Product Restaurant info', $fields_product, 'product', true );
-$post_meta_box = new custom_add_meta_box( 'post_meta_box', 'Product options', $fields_page, array('product'), true );
+$post_meta_box = new custom_add_meta_box( 'post_meta_box', 'Product options', $product_fields, array('product'), true );
+$page_meta_box = new custom_add_meta_box( 'page_meta_box', 'Page options', $page_fields, 'page', true );
 
 
--- wp-content/themes/leflair/template-about2.php
+++ wp-content/themes/leflair/template-about2.php
@@ -1,19 +1,21 @@
 <?php
 /*
-Template Name: About 2
+Template Name: About
 */
+
+$page_id= get_the_ID();
 ?>
 <?php while (have_posts()) : the_post(); ?>
 			
 	<!-- uiView:  -->
 	<section>
 		<section class="section-info">
-			<div class="hero about-hero aligner inverted" style="background-image: url(<?php echo site_url('wp-content/themes/leflair/assets/img/hero-careers.png');?>); background-position: 50% 50%; background-repeat: no-repeat; background-size: 1350px;">
+			<div class="hero about-hero aligner inverted" style="background-image: url(<?php echo wp_get_attachment_url(get_post_meta($page_id, 'page_header_image', true));?>);">
 				<div class="container aligner-item">
 					<div class="row">
 						<div class="col-lg-12 text-center hero-center-callout">
-							<h2><?php echo roots_title(); ?></h2>
-							<p class="lead">Vì sao bạn nên gia nhập Leflair thay vì một trong hàng triệu công ty khác?</p>
+							<h2><?php echo get_post_meta($page_id, 'page_title', true); ?></h2>
+							<p class="lead"><?php echo get_post_meta($page_id, 'page_subtitle', true);?></p>
 						</div>
 					</div>
 				</div>
@@ -24,10 +26,9 @@ Template Name: About 2
 			<div class="row">
 			
 				<div class="col-lg-6 col-lg-offset-3">
-					<h2 class="section-header"><span>Title</span></h2>
-					<p>
+					<div class="about-content">
 						<?php the_content(); ?>
-					</p>
+					</div>
 				</div>
 				
 			</div>
